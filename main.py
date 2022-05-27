516a2e3ec7396911bf95f9c7658c06c610c2968d
tdunlap607 <tdunlap607@gmail.com>
2022-05-27 09:26:26 -0400

    Minor updates

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import pytorch_lightning
-from pytorch_lightning import Trainer
 import yaml
 
 if __name__ == '__main__':
     print("the coolest project ever")
+
+    print(yaml.dump({'coolProject': 'yes'}))
+
+    pytorch_lightning_version = pytorch_lightning.__version__
+
+    print(pytorch_lightning_version)
 
     print("done")
